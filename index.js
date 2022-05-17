82d21f9c03a71df32f866b04f7e98a5bf2d80474
Joel Roed <jooel.roed@gmail.com>
2022-05-17 18:29:37 +0200

    Button CSS

--- index.js
+++ index.js
@@ -8,10 +8,14 @@ form.addEventListener('submit',function(event) {
     document.querySelector('#output__pre').style.opacity = "0";
 
     var note = document.querySelector('#input__note--id').value;
-    document.querySelector("#output__main").innerHTML += `<div class="output__note">
-    <h3>Note ${counterValue}</h3>
-    <p>${note}</p>
-    BTN
-  </div>`
+    document.querySelector("#output__main").innerHTML += `<div class="output__wrapper"><div class="output__note">
+    <h3 class="output__note--title">Note ${counterValue}</h3>
+    <p class="output__note--act">${note}</p>
+    <div>
+    <button class="button open-button"> View Details </button>
+    </div>
+  </div>
+  </div>
+  `
 
 })
